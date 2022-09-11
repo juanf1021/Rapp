ad9ff08f8694f4fb3d426afee7027afa34a55f99
juanf1021 <82191280+juanf1021@users.noreply.github.com>
2022-09-11 14:45:38 -0500

    Images database created and migrated

--- Rapp/play/admin.py
+++ Rapp/play/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Words
+from .models import CategoriaImagen, Images, Words
 # Register your models here.
 
 class Words_admin(admin.ModelAdmin):
     search_fields= ("word",)
 
 admin.site.register(Words, Words_admin)
+admin.site.register(Images)
+admin.site.register(CategoriaImagen)
+
--- Rapp/play/models.py
+++ Rapp/play/models.py
@@ -2,11 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
-from django.db import models
-
-# Create your models here.
-
 class Words(models.Model):
     word = models.CharField(max_length=100, unique=True)
     definition = models.CharField(max_length=800) 
@@ -16,3 +11,29 @@ class Words(models.Model):
 
     def __str__(self):
         return self.word
+    
+
+class CategoriaImagen(models.Model):
+    categoria = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name= "Categoria"
+        verbose_name_plural= "Categorias"
+
+    def __str__(self):
+        return self.categoria
+
+
+class Images(models.Model):
+    name=models.CharField(max_length=100)
+    categoria = models.ForeignKey(CategoriaImagen, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    image = models.FileField(upload_to='images')
+    class Meta:
+        verbose_name= "Image"
+        verbose_name_plural= "Images"
+
+    def __str__(self):
+        return self.name
+
+
